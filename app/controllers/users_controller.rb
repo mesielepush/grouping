def1d50a63c48a6bc145399a69ad975ba77a3ae7
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-02-28 16:18:21 -0600

    bootstrap added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!
     def index
+        @users = User.all
     end
     def show
+        @user = User.find(current_user.id)
     end
 end
