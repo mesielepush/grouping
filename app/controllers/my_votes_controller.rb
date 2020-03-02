3e2b130af58526e69b6287986d145c820e3bca88
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-03-01 21:54:50 -0600

    Update my_votes_controller and check main logic

--- app/controllers/my_votes_controller.rb
+++ app/controllers/my_votes_controller.rb
@@ -1,2 +1,20 @@
 class MyVotesController < ApplicationController
+    def create
+        @my_vote = current_user.my_votes.new(post_params)
+
+        if @my_vote.save
+            
+            notice: 'Your Vote was successfully created.'
+        else
+            
+            render :index, alert: 'Vote was not created.'
+        end
+    end
+    def show
+        @my_vote = Vote.find(params[:id])
+    end
+
+    def post_params
+        params.require(:post).permit(:content)
+      end
 end
