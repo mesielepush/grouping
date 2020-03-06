17cbb51387e4fd6aa0e9042b591b3a4808b07fb9
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-03-05 19:18:22 -0600

    group analaize data views

--- app/models/group.rb
+++ app/models/group.rb
@@ -10,8 +10,50 @@
 #  description :string
 #  imagen_url  :string
 #
-
+require 'set'
 class Group < ApplicationRecord
     belongs_to :user
     has_many :votes
+    
+    def group_total
+        total = 0
+        demands = self.votes
+        demands.each do |demand|
+            total += demand.counter
+        end
+        total
+    end
+
+    def my_collaborators(current_user)
+        collaborators = {}
+        demands = self.votes.where.not(user_id: current_user)
+
+        demands.each do |demand|
+            
+            collaborators[demand.user_id] ||= {}
+            collaborators[demand.user_id][:counter] = 0
+            collaborators[demand.user_id][:name] = User.find_by_id(demand.user_id).name
+            collaborators[demand.user_id][:counter] += demand.counter
+            puts '###################################'
+            puts collaborators.keys
+            puts '###################################'
+
+        end
+        
+        collaborators
+    end
+
+    def all_collaborators
+        colaborators = []
+        demands = self.votes
+        demands.each do |demand|
+            collaborators[demand.user_id] ||= {}
+            collaborators[demand.user_id][:counter] = 0
+            collaborators[demand.user_id][:creator] = demand.user_id
+            collaborators[demand.user_id][:counter] += demand.counter
+        end
+        collaborators
+    end
+
+
 end
