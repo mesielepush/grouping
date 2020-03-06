5ad9be352c13cdadf6cb6d984c5efd62a8f87793
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-03-06 17:09:15 -0600

    group_stats done

--- app/controllers/my_votes_controller.rb
+++ app/controllers/my_votes_controller.rb
@@ -11,15 +11,13 @@ class MyVotesController < ApplicationController
             render :index, alert: 'Vote was not created.'
         end
     end
-    def show
-        if params[:group_id] != nil
-            return @group = Group.find_by_id(params[:group_id])
-        else
-            return @vote = Vote.find_by_id(params[:vote_id])
-        end
 
+    def show_group
+        @group = Group.find_by_id(params[:group_id])
     end
-
+    def show
+        @votes = Vote.find_by_id(params[:vote_id])
+    end
     def index
         
     end
--- config/routes.rb
+++ config/routes.rb
@@ -65,5 +65,6 @@ Rails.application.routes.draw do
   post '/groups_new',   to: 'groups#create'
   get '/demands_new',   to: 'votes#new'
   post '/demands_new',   to: 'votes#create'
-  get '/group_stats', to: 'my_votes#show'
+  get '/group_stats', to: 'my_votes#show_group'
+  get '/vote_stats', to: 'my_votes#show'
 end
