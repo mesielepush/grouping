0d1826573b7f97ce593505ab334f254357b8e0bb
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-03-11 16:08:52 -0600

    freq data function done
    data frequency for the demands of a group it's now being sending correctly from my_votes_show to the my_votes_show_url

--- app/controllers/my_votes_controller.rb
+++ app/controllers/my_votes_controller.rb
@@ -16,8 +16,23 @@ class MyVotesController < ApplicationController
         @group = Group.find_by_id(params[:group_id])
     end
     def show
-        
-        @votes = Vote.find_by_id(params[:vote_id])
+        if params[:group_id]
+           @group = Group.find_by_id(params[:group_id])
+           @bar_data = [ ['Demand', 'Votes'] ]
+           organinzer = {} 
+           
+           @group.votes.each do |vote|
+             organinzer[vote.name] = vote.counter
+            end
+           organinzer = organinzer.sort_by {|k, v| v}
+
+           organinzer.each do |name,votes|
+            @bar_data << [name,votes]
+           end
+           
+           return @bar_data
+           
+        end
     end
     def index
         
