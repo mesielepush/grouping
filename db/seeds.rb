017010c8318aafa5026e3c3906f76ec0b46e2e39
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-03-10 19:49:33 -0600

    critical mistake took all day, I never knew what happened
    Some group object from the @groups variable was nil for some reason, it all happened with the manipulation of the seeds functions

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,33 +7,89 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-user1 = User.new(name: 'uno', email: 'uno@gmail.com', gravatar: 'https://bit.ly/3cvbjKU', password: 'pppppp', password_confirmation: 'pppppp')
-user2 = User.new(name: 'dos', email: 'dos@gmail.com', gravatar: 'https://go.aws/2uR55UX',password: 'pppppp', password_confirmation: 'pppppp')
-user3 = User.new(name: 'tres', email: 'tres@gmail.com', gravatar: 'https://bit.ly/3csJgMs',password: 'pppppp', password_confirmation: 'pppppp')
-user4 = User.new(name: 'cuatro', email: 'cuatro@gmail.com', gravatar: 'https://bit.ly/32LOrCB',password: 'pppppp', password_confirmation: 'pppppp')
-user5 = User.new(name: 'cinco', email: 'cinco@gmail.com', gravatar: 'https://bit.ly/32LOCOh',password: 'pppppp', password_confirmation: 'pppppp')
-user6 = User.new(name: 'seis', email: 'seis@gmail.com', gravatar: 'https://bit.ly/32NamJz',password: 'pppppp', password_confirmation: 'pppppp')
+user1 = User.new(name: 'one', email: 'one@gmail.com', gravatar: 'https://bit.ly/3cvbjKU', password: 'pppppp', password_confirmation: 'pppppp')
+user2 = User.new(name: 'two', email: 'two@gmail.com', gravatar: 'https://go.aws/2uR55UX',password: 'pppppp', password_confirmation: 'pppppp')
+user3 = User.new(name: 'three', email: 'three@gmail.com', gravatar: 'https://bit.ly/3csJgMs',password: 'pppppp', password_confirmation: 'pppppp')
+user4 = User.new(name: 'four', email: 'four@gmail.com', gravatar: 'https://bit.ly/32LOrCB',password: 'pppppp', password_confirmation: 'pppppp')
+user5 = User.new(name: 'five', email: 'five@gmail.com', gravatar: 'https://bit.ly/32LOCOh',password: 'pppppp', password_confirmation: 'pppppp')
+user6 = User.new(name: 'six', email: 'six@gmail.com', gravatar: 'https://bit.ly/32NamJz',password: 'pppppp', password_confirmation: 'pppppp')
 
-user1.save
-user2.save
-user3.save
-user4.save
-user5.save
-user6.save
+if user1.save
+    puts '##################################'
+    puts 'User one DONE'
+    puts '##################################'
+end
+
+if user2.save
+    puts '##################################'
+    puts 'User one 2'
+    puts '##################################'
+end
+
+if user3.save
+    puts '##################################'
+    puts 'User one 3'
+    puts '##################################'
+end
+
+if user4.save
+    puts '##################################'
+    puts 'User one 4'
+    puts '##################################'
+end
+
+if user5.save
+    puts '##################################'
+    puts 'User one 5'
+    puts '##################################'
+end
+
+if user6.save
+    puts '##################################'
+    puts 'User one 6'
+    puts '##################################'
+end
+
 
 user_group_1 = user1.groups.new(name: "Kiwi's Education", description: "Having a Kiwi i'ts cool and all but what about the kikwi's learning environment?.", imagen_url: 'https://bit.ly/3cBnxlB')
 user_group_2 = user2.groups.new(name: 'General Relativity', description: "Is it?, is it thou?", imagen_url: 'https://bit.ly/39iZPII')
-user_group_3 = user3.groups.new(name: 'Jacques Brel is trash', description: 'Jacques Brel should be prohibited, he smells funny, maybe drinks some alcohols or something', imagen_url: 'https://bit.ly/2vHKgeO')
+user_group_3 = user3.groups.new(name: 'Jacques Brel', description: 'Jacques Brel should be prohibited, he smells funny, maybe drinks some alcohols or something', imagen_url: 'https://bit.ly/2vHKgeO')
 user_group_4 = user4.groups.new(name: 'Bruno did nothing wrong', description: 'Even thou Mr Giordano i´ts not very popular right now, we should check his non secular works for memes' , imagen_url: 'https://bit.ly/2TyeHMB')
 user_group_5 = user5.groups.new(name: 'Apples', description: 'apples', imagen_url: 'https://bit.ly/32XtrZT')
 user_group_6 = user6.groups.new(name: 'Victory of Samothrace', description: 'Self explanatory mate', imagen_url: 'https://bit.ly/3aredyt')
 
-user_group_1.save
-user_group_2.save
-user_group_3.save
-user_group_4.save
-user_group_5.save
-user_group_6.save
+if user_group_1.save
+    puts '##################################'
+    puts 'User one 6'
+    puts '##################################'
+end
+if user_group_2.save
+    puts '##################################'
+    puts 'User one 6'
+    puts '##################################'
+end
+if user_group_3.save
+    puts '##################################'
+    puts 'User one 6'
+    puts '##################################'
+end
+
+if user_group_4.save
+    puts '##################################'
+    puts 'User one 6'
+    puts '##################################'
+end
+if user_group_5.save
+    puts '##################################'
+    puts 'User one 6'
+    puts '##################################'
+end
+
+if user_group_6.save
+    puts '##################################'
+    puts 'User one 6'
+    puts '##################################'
+end
 
 groups = [user_group_1,user_group_2,user_group_3,user_group_4,user_group_5,user_group_6,]
 
@@ -80,7 +136,7 @@ end
 
 
 groups.each do |group|
-    (1..50).each do
+    (1..10).each do
         
         demand = makeDemand(group.id)
         
