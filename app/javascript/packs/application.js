a187622ab714eb5df9c05f3cf7b2c171a849265b
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-03-13 16:39:45 -0600

    minor changes trying to put js code inside the rails pack, spoiler alert: I could not
    I have some Js code inside the my_votes.show view but it wont compile for heroku unless is packed with the rails js packs. But I'm also passing some data from the my_vote controllers to the JS functions, so... I don't know how to make it work.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,11 +9,16 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 
+
 import 'bootstrap'
 import "../stylesheets/application"
+
 document.addEventListener('turbolinks:load', () => {
     $('[data-toggel="tooltip"]').tooltip()
     $('[data-toggel="popover"]').popover()
+
+    
+
 })
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
