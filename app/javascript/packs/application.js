d5e193363d317e85ed696e799700678c476c65ef
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-03-16 13:49:31 -0600

    changes for the heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ require("channels")
 require("jquery")
 
 import 'bootstrap'
-import "../assets/stylesheets/application.scss"
+import "../stylesheets/application"
 document.addEventListener('turbolinks:load', () => {
     $('[data-toggel="tooltip"]').tooltip()
     $('[data-toggel="popover"]').popover()
